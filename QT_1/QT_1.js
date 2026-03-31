5e014cb79d7a519dff0341c5d5d1b70c35c38818
Aditya <adityachoudhary0221@gmail.com>
2026-03-31 15:15:37 +0530

    two sum solution

--- QT_1/QT_1.js
+++ QT_1/QT_1.js
@@ -14,7 +14,17 @@
  */
 
 function twoSum(nums, target) {
-    //Write you Code here
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+
+        if (map.has(diff)) {
+            return [map.get(diff), i];
+        }
+
+        map.set(nums[i], i);
+    }
 
     return [];
 }
