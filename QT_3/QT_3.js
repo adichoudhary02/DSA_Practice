45f2f63efaeeffc60eeed2bb8a3b0c377e97ddfe
Aditya <adityachoudhary0221@gmail.com>
2026-04-01 15:15:35 +0530

    solution- bit toggling- optimal solution

--- QT_3/QT_3.js
+++ QT_3/QT_3.js
@@ -17,52 +17,64 @@
  * 1 <= N <= 100
  */
 
-let binNum = 0;
-let binNum2 = 0
+// let binNum = 0;
+// let binNum2 = 0
 
-function binaryNum(n, dig=1){
-        if(n>0){
-            binNum = binNum + (n%2)*dig;
-            return binaryNum(Math.floor(n/2),dig*=10);
-        }
-        return binNum;
-    }
+// function binaryNum(n, dig=1){
+//         if(n>0){
+//             binNum = binNum + (n%2)*dig;
+//             return binaryNum(Math.floor(n/2),dig*=10);
+//         }
+//         return binNum;
+//     }
 
-function toggleBinaryBits(n, dig=1){
-    if(n>0){
-        binNum2 = binNum2 + (((n+1)%2)%10)*dig;
-        return toggleBinaryBits(Math.floor(n/10),dig *= 10);
-    }
-    return binNum2;
+// function toggleBinaryBits(n, dig=1){
+//     if(n>0){
+//         binNum2 = binNum2 + (((n+1)%2)%10)*dig;
+//         return toggleBinaryBits(Math.floor(n/10),dig *= 10);
+//     }
+//     return binNum2;
     
-}
+// }
 
-function binaryToDec(n){
-    let decNum = 0, dig = 1;
-    while(n>0){
-        decNum = decNum + (n%10)*dig;
-        dig = 2*dig;
-        n = Math.floor(n/10);
-    }
-    return decNum;
-}
+// function binaryToDec(n){
+//     let decNum = 0, dig = 1;
+//     while(n>0){
+//         decNum = decNum + (n%10)*dig;
+//         dig = 2*dig;
+//         n = Math.floor(n/10);
+//     }
+//     return decNum;
+// }
 
 function toggleBits(n) {
+    //Approach 1
     {/*let bigMultiple = 2;
     while(bigMultiple<=n){
         bigMultiple*=2;
     }
     return (bigMultiple-1)-n;*/}
 
-    binNum = 0;
-    binNum2 = 0;
+    //Approach 2
+    // binNum = 0;
+    // binNum2 = 0;
 
-    n = binaryNum(n);
-    n = toggleBinaryBits(n);
-    n = binaryToDec(n);
+    // n = binaryNum(n);
+    // n = toggleBinaryBits(n);
+    // n = binaryToDec(n);
 
-    return n;
+    // return n;
+    
+    //Approach 3: Optimal
 
+    let power = 1;
+
+    while(n>=power){
+        power = power<<1;
+    }
+
+    return n ^ (power-1);
+    
 }
 
 module.exports = { toggleBits };
