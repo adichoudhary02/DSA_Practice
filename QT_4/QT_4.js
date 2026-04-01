71343c978d79a9b6dbb7c75799a931d260b2cbad
Aditya <adityachoudhary0221@gmail.com>
2026-04-01 16:48:07 +0530

    solution- no. of sundays

--- QT_4/QT_4.js
+++ QT_4/QT_4.js
@@ -6,23 +6,38 @@
  * 
  * Input:
  * - startDay (string): "mon", "tue", ..., "sun"
- * - n (integer): number of days
+ * - n (integer): next number of days
  * 
  * Output:
- * - number of Sundays in those n days
+ * - number of Sundays in those next n days
  * 
  * Example:
  * Input: startDay = "mon", n = 13
  * Output: 2
  * 
  * Explanation:
- * mon → first Sunday after 6 days
+ * mon → first Sunday after next 6 days
  * remaining days allow one more Sunday
  */
 
 function countSundays(startDay, n) {
-    // ✍️ Your code here
+    let weekDay = ['sun', 'sat', 'fri', 'thu', 'wed', 'tue', 'mon'];
+    let dayIndex = 0, sundays = 0;
 
+    for(let i = 0; i<weekDay.length; i++){
+        if(weekDay[i] == startDay){
+            dayIndex = i;
+        }
+    }
+    if ((n = n-dayIndex) > 0) 
+        sundays++;
+
+    while(n >= 7){
+        sundays++;
+        n -= 7;
+    }
+
+    return sundays;
 }
 
 module.exports = { countSundays };
