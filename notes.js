6302345ac3b29a6b3cf7c962a2b4845318b02939
Aditya <adityachoudhary0221@gmail.com>
2026-04-01 17:57:20 +0530

    map syntax - basic

--- notes.js
+++ notes.js
@@ -13,3 +13,15 @@ n = n>>2; //this divise by 3 twice  `10101` becomes `101`
 //Result is always in the decimal formate...
 
 //n^b this convers all the decimal number into the binary then takes xors and then conver the result back to decimal..
+
+//How to create map and then use them with key value pair// ablity to find them in O(1)
+let map = new Map();
+    map = {
+        'mon': 0,
+        'tue': 1,
+        'wed': 2,
+        'thu': 3,
+        'fri': 4,
+        'sat': 5,
+        'sun': 6
+    }
